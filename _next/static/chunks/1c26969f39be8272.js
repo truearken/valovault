51b5d9459760ce5e505c8d96235269c31c8a0c58
arkeN <vodelsup@gmail.com>
2025-10-03 15:21:01 +0200

    Updates

---
+++ _next/static/chunks/1c26969f39be8272.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/66a011739b913cf5.js",
+  "static/chunks/turbopack-1d03fda71a93b04a.js"
+])
---
+++ _next/static/chunks/edee78b6728764f4.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/66a011739b913cf5.js",
+  "static/chunks/turbopack-91198d79a00ca36e.js"
+])
