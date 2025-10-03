59858866d528f834a283d5adb8da2041d665afe1
arkeN <vodelsup@gmail.com>
2025-10-03 12:57:40 +0200

    Updates

---
+++ _next/static/hqD4bOgjHmd5m6USIVQ0b/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/f6b352e3b732575b.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/valovault//_next/:path+",
+        "destination": "/valovault/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
