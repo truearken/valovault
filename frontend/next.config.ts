9b0fcddc7c2426adf131d55b367b9b89378f8dbc
arkeN <vodelsup@gmail.com>
2025-10-03 14:56:36 +0200

    fix img deployment

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/valovault',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
