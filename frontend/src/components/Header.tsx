b262babb2b8dfd85c43a59d594cb2feb8bdcaaad
arkeN <vodelsup@gmail.com>
2025-10-10 19:36:47 +0200

    dark mode to header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,16 +1,29 @@
-type HeaderProps = {
-  isEditing: boolean;
-};
+"use client";
 
-export default function Header({ isEditing }: HeaderProps) {
+import { useTheme } from "@/context/ThemeContext";
+
+type HeaderProps = {};
+
+export default function Header({}: HeaderProps) {
+  const { theme, toggleTheme } = useTheme();
+  const isDarkMode = theme === 'dark';
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+    <nav className="navbar navbar-expand-lg sticky-top bg-body-tertiary">
       <div className="container">
         <span className="navbar-brand">ValoVault</span>
         <div className="ms-auto">
-          {!isEditing && (
-            <></>
-          )}
+          <div className="form-check form-switch">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              role="switch"
+              id="darkModeSwitchHeader"
+              checked={isDarkMode}
+              onChange={toggleTheme}
+            />
+            <label className="form-check-label" htmlFor="darkModeSwitchHeader">Dark Mode</label>
+          </div>
         </div>
       </div>
     </nav>
--- frontend/src/components/SettingsCard.tsx
+++ frontend/src/components/SettingsCard.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { useTheme } from "@/context/ThemeContext";
-
 type SettingsCardProps = {
     autoSelectAgent?: boolean;
     onAutoSelectAgentChange: (value: boolean) => void;
@@ -9,26 +7,12 @@ type SettingsCardProps = {
 };
 
 export default function SettingsCard({ autoSelectAgent, onAutoSelectAgentChange, isLoading }: SettingsCardProps) {
-    const { theme, toggleTheme } = useTheme();
-    const isDarkMode = theme === 'dark';
-
     return (
         <div className="p-3 border mb-3">
             <h2 className="mb-3">Settings</h2>
             <div className="form-check form-switch mb-2">
                 <input className="form-check-input" type="checkbox" role="switch" id="autoSelectAgentSwitch" checked={autoSelectAgent || false} onChange={(e) => onAutoSelectAgentChange(e.target.checked)} disabled={isLoading} />
                 <label className="form-check-label" htmlFor="autoSelectAgentSwitch">Auto-select agent-specific loadout</label>
-            </div>
-            <div className="form-check form-switch mb-2">
-                <input
-                    className="form-check-input"
-                    type="checkbox"
-                    role="switch"
-                    id="darkModeSwitch"
-                    checked={isDarkMode}
-                    onChange={toggleTheme}
-                />
-                <label className="form-check-label" htmlFor="darkModeSwitch">Dark Mode</label>
             </div>
         </div>
     );
