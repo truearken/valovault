3a745ec0c90f563b9031ab85638ebd8e44124a65
arkeN <vodelsup@gmail.com>
2025-10-10 19:50:04 +0200

    fixed header error

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,9 +2,7 @@
 
 import { useTheme } from "@/context/ThemeContext";
 
-type HeaderProps = {};
-
-export default function Header({ }: HeaderProps) {
+export default function Header() {
     const { theme, toggleTheme } = useTheme();
     const isDarkMode = theme === 'dark';
 
@@ -17,8 +15,7 @@ export default function Header({ }: HeaderProps) {
                         className="btn btn-outline-secondary d-flex align-items-center justify-content-center"
                         onClick={toggleTheme}
                         style={{ width: '2.5rem', height: '2.5rem', padding: 0 }}
-                        title="Toggle theme"
-                    >
+                        title="Toggle theme">
                         {isDarkMode ? "L" : "D"}
                     </button>
                 </div>
