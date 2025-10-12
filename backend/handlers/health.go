4d9c75b78b23b32100551673fb142a5f4b7c0689
arkeN <vodelsup@gmail.com>
2025-10-12 19:52:33 +0200

    health checks

--- backend/handlers/health.go
+++ backend/handlers/health.go
@@ -1,9 +1,22 @@
 package handlers
 
-import "net/http"
+import (
+	"log/slog"
+	"net/http"
+	"strings"
+
+	"github.com/truearken/valclient/valclient"
+)
 
 func (h *Handler) Health(w http.ResponseWriter, r *http.Request) {
 	if _, err := h.Val.GetLocalWebsocket(); err != nil {
+		slog.Info("health error", "err", err)
+		if strings.Contains(err.Error(), "No connection could be made because the target machine actively refused it") {
+			newVal, _ := valclient.NewClient()
+			if newVal != nil {
+				h.Val = newVal
+			}
+		}
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
