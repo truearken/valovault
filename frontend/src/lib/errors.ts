5202c1fb3f6026c72bdb7c0009b0e53c981a656b
arkeN <vodelsup@gmail.com>
2025-10-20 23:29:57 +0200

    upgrade version

--- frontend/src/lib/errors.ts
+++ frontend/src/lib/errors.ts
@@ -1,7 +1,7 @@
 export class LocalClientError extends Error {
   constructor() {
+    super();
     this.message = 'Could not connect to the local client. Please make sure it is running and try again.'
-    super(message);
     this.name = 'LocalClientError';
   }
 }
