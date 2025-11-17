63ddaf2c6320a75c0f5a360dac958e7b17089d84
arkeN <vodelsup@gmail.com>
2025-11-17 23:23:52 +0100

    fixed a ressource leak in go backend

--- backend/go.mod
+++ backend/go.mod
@@ -2,6 +2,6 @@ module backend
 
 go 1.25.1
 
-require github.com/truearken/valclient v0.1.0
+require github.com/truearken/valclient v0.1.1
 
 require github.com/gorilla/websocket v1.5.3 // indirect
--- backend/handlers/health.go
+++ backend/handlers/health.go
@@ -8,12 +8,14 @@ import (
 )
 
 func (h *Handler) Health(w http.ResponseWriter, r *http.Request) {
-	if _, err := h.Val.GetLocalWebsocket(); err != nil {
+	if _, err := h.Val.GetHelp(); err != nil {
 		newVal, _ := valclient.NewClient()
 		if newVal != nil {
 			slog.Info("valorant started, new client created")
 			h.Val = newVal
 		}
+		defer h.Val.Close()
+
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
